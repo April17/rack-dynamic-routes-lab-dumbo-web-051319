c87fb2578d8ba3edda4e23355a2264c7f3fa7003
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-02 20:50:52 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,21 +1,20 @@
 class Application
 
-   @@songs = [Song.new("Sorry", "Justin Bieber"),
-             Song.new("Hello","Adele")]
+  @@songs = [Song.new("Sorry", "Justin Bieber"),
+            Song.new("Hello","Adele")]
 
-   def call(env)
-     resp = Rack::Response.new
-     req = Rack::Request.new(env)
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
 
-     if req.path.match(/songs/)
+    if req.path.match(/songs/)
 
-       song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
-       song = @@songs.find{|s| s.title == song_title}
+      song_title = req.path.split("/songs/").last #turn /songs/Sorry into Sorry
+      song = @@songs.find{|s| s.title == song_title}
 
-       resp.write song.artist
-     end
+      resp.write song.artist
+    end
 
-     resp.finish
-   end
-
+    resp.finish
+  end
 end
