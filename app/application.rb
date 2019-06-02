a23fbf2f667b67f2a591b662e244d94a73553c73
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-02 20:55:01 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -11,6 +11,9 @@ class Application
       item = @@items.find{|item| item.name == item_name}
 
       resp.write item.price
+    else
+      resp.write "Item not found"
+      resp.status = 400
     end
 
     resp.finish
