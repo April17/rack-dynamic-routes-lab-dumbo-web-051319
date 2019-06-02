0f3feccda4389079f616f94da002b7d9e9b07643
Fanqiang Meng <fanmeng0417@gmail.com>
2019-06-02 20:58:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,7 +7,7 @@ class Application
 
     if req.path.match(/items/)
 
-      item_name = req.path.split("/items/").last #turn /songs/Sorry into Sorry
+      item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
 
       resp.write item.price
